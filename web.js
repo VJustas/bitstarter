b43cb8d66c18fc9d787c387f1e47925cddab53b5
VJustas <jvarteckas@gmail.com>
2013-07-08 20:37:56 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function (request, response) {
-    response.send('Hello World 2!');
+    var res = fs.readFileSync('index.html', 'utf8'); 
+    response.send(res);
 });
 
 var port = process.env.PORT || 5000;
